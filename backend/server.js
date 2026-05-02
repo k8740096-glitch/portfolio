e0deaf9d63e336efe3393e073be0235c354004a1
Khushboo <k8740096gmail.com>
2026-05-02 10:11:55 +0530

    fix route wildcard error

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get("/",(req, res) => {
 //server static files
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*",(req, res)=> {
+app.get("/*",(req, res)=> {
         res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
